d4e7c31212cd79359634f15d9ad3b67ded898f5d
Vova <badukgame@ukr.net>
2023-03-01 20:47:39 +0200

    Contact

--- crypto_root/blog/admin.py
+++ crypto_root/blog/admin.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 from django.contrib.auth.models import User
 
-from .models import Blog,Article,Comment,UserVote
+from .models import Blog,Article,Comment,UserVote,Contact
 # Register your models here.
 admin.site.register(Blog)
 admin.site.register(Article)
 admin.site.register(Comment)
 admin.site.register(UserVote)
+admin.site.register(Contact)
